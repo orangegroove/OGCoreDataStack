16df0fe073ab8c86b5a491e5fa5e889976cb5754
Jesper Skrufve <jesper.skrufve@gmail.com>
2013-08-16 13:42:49 +0300

    Initial Commit

---
+++ OGObjectLayerProject/OGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OGAppDelegate.h
+//  OGObjectLayerProject
+//
+//  Created by Jesper on 8/16/13.
+//  Copyright (c) 2013 Orange Groove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OGObjectLayerProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OGObjectLayerProject
+//
+//  Created by Jesper on 8/16/13.
+//  Copyright (c) 2013 Orange Groove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([OGAppDelegate class]));
+	}
+}
